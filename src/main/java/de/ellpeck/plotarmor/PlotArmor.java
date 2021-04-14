21ff26cedd28ae13a243a40efc1938a1e0276639
Ellpeck <me@ellpeck.de>
2021-04-14 21:54:22 +0200

    basic infrastructure

---
+++ src/main/java/de/ellpeck/plotarmor/PlotArmor.java
@@ -0,0 +1,12 @@
+package de.ellpeck.plotarmor;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = PlotArmor.ID, name = PlotArmor.NAME, version = PlotArmor.VERSION)
+public class PlotArmor {
+
+    public static final String ID = "plotarmor";
+    public static final String NAME = "Plot Armor";
+    public static final String VERSION = "@VERSION@";
+
+}
